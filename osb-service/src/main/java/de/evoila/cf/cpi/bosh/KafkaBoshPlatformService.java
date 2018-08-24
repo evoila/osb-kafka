1220a2b61152261c6cfde7e2e8eb6e7a94a9e123
Johannes Strauß <jstrauss@evoila.de>
2018-08-24 10:41:36 +0200

    fix(osb-service): KafkaBoshPlatformService
    fix ports for proper ServicePortAvailabilityVerifier testing.

---
+++ osb-service/src/main/java/de/evoila/cf/cpi/bosh/KafkaBoshPlatformService.java
@@ -0,0 +1,77 @@
+package de.evoila.cf.cpi.bosh;
+
+import de.evoila.cf.broker.bean.BoshProperties;
+import de.evoila.cf.broker.exception.PlatformException;
+import de.evoila.cf.broker.model.DashboardClient;
+import de.evoila.cf.broker.model.Plan;
+import de.evoila.cf.broker.model.ServerAddress;
+import de.evoila.cf.broker.model.ServiceInstance;
+import de.evoila.cf.broker.repository.PlatformRepository;
+import de.evoila.cf.broker.service.CatalogService;
+import de.evoila.cf.broker.service.availability.ServicePortAvailabilityVerifier;
+import io.bosh.client.deployments.Deployment;
+import io.bosh.client.errands.ErrandSummary;
+import io.bosh.client.tasks.Task;
+import io.bosh.client.vms.Vm;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import rx.Observable;
+
+@Service
+@ConditionalOnBean(BoshProperties.class)
+public class KafkaBoshPlatformService extends BoshPlatformService {
+
+    private static final int KAFKA_PORT = 9092;
+    private static final int ZOOKEEPER_PORT = 2181;
+    private static final String KAFKA_JOB_NAME = "osb-bosh-kafka";
+    ;
+
+
+    KafkaBoshPlatformService(PlatformRepository repository, CatalogService catalogService, ServicePortAvailabilityVerifier availabilityVerifier, BoshProperties boshProperties, Optional<DashboardClient> dashboardClient) {
+        super(repository, catalogService, availabilityVerifier, boshProperties, dashboardClient, new MongoDBDeploymentManager(boshProperties));
+    }
+
+    public void runCreateErrands(ServiceInstance instance, Plan plan, Deployment deployment, Observable<List<ErrandSummary>> errands) throws PlatformException {
+        Task task = super.connection.connection().errands().runErrand(deployment.getName(), "kafka-smoke-tests").toBlocking().first();
+        super.waitForTaskCompletion(task);
+
+    }
+
+    protected void runUpdateErrands(ServiceInstance instance, Plan plan, Deployment deployment, Observable<List<ErrandSummary>> errands) throws PlatformException {
+//        Task task = super.connection.connection().errands().runErrand(deployment.getName(), "reconfigure-replset").toBlocking().first();
+        //       super.waitForTaskCompletion(task);
+    }
+
+    protected void runDeleteErrands(ServiceInstance instance, Deployment deployment, Observable<List<ErrandSummary>> errands) {
+    }
+
+    @Override
+    protected void updateHosts(ServiceInstance in, Plan plan, Deployment deployment) {
+
+        List<Vm> vms = connection.connection().vms().listDetails(BoshPlatformService.DEPLOYMENT_NAME_PREFIX + in.getId()).toBlocking().first();
+        if (in.getHosts() == null)
+            in.setHosts(new ArrayList<>());
+
+        in.getHosts().clear();
+
+        vms.forEach(vm -> {
+            ServerAddress serverAddress;
+
+            if (vm.getJobName().equals(KAFKA_JOB_NAME)) {
+                serverAddress = new ServerAddress("Host-" + vm.getIndex(), vm.getIps().get(0), KAFKA_PORT);
+            } else {
+                serverAddress = new ServerAddress("Host-" + vm.getIndex(), vm.getIps().get(0), ZOOKEEPER_PORT);
+            }
+
+            in.getHosts().add(serverAddress);
+        });
+    }
+
+    @Override
+    public void postDeleteInstance(ServiceInstance serviceInstance) throws PlatformException {
+    }
+}
